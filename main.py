9996c884be15403752a521c1a4094d91f3045fb0
Sura Liu <55587600+gutongww@users.noreply.github.com>
2026-02-08 15:29:14 +1300

    change to bark

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import math
 # ================= Bark 配置 =================
 BARK_KEYS = [
     "oQZ92sPHphNF6D7fNvYawR",
-    "uBSGy5uo7yrbb6JknU4B5F"
+    "uBSGy5uo7yrbb6JknU4B5F"  
 ]
 
 BARK_APIS = [f"https://api.day.app/{key}" for key in BARK_KEYS]
@@ -22,14 +22,15 @@ HER_BIRTHDAY = "02-20"  # MM-DD
 DAILY_RANDOM_WORDS = [
     "昭昭心许云深处 岁岁情归峥骨间",
     "娘娘就是要我的命 我眼都不会眨一下 自会取剑剖心",
-    "臣在",
     "宁宁 我们什么时候能一起回草原看望春花",
     "你的正面与负面 我全部痴迷",
+    "老派约会啊 那我们要一路沿着珠江边走 毫无顾忌地饮酒 话题要漫无目的 像鱼在海里游泳"
+    "在最后一个路口 我们会仰望绵延千里的骑楼街 去底下的一家咖啡店买两杯馥芮白 在等待窗口站五分钟 我们会无聊 所以我会碰碰你的肩 你会摸摸我的头 而这样细微的触动 让五分钟的等待有了无限意义"
     "你是我暗室逢灯的际遇 绝渡逢舟的功德",
     "既然这个世界没有真正的感同，那我就身受",
     "珠水不必美 天地之宽 容得下你的不喜欢",
+    "试着接受我吧 人生在正确与错误之外 还有爱啊",
     "我们在相同的频道里",
-    "很多人在四通八达的巷子走散 却能在下一个巷口的青砖屋前相遇 如果你愿意 我们以后就定居在那里 如果你不愿意 你想去哪里 我都陪你",
     "在你可寻找的范围内 我一直在",
 ]
 
@@ -57,13 +58,13 @@ def get_weather():
 # ================= 温度关心语 =================
 def temp_care_words(temp):
     if temp <= 12:
-        return "今天有点冷 记得多穿一点 "
+        return "今天有点冷 要多穿一点 小心着凉"
     elif 13 <= temp <= 20:
         return "今天的温度刚刚好 "
-    elif 21 <= temp <= 26:
-        return "今天有点暖 记得多喝水 "
+    elif 21 <= temp <= 24:
+        return "今天有点热 记得多喝水 "
     else:
-        return "今天可能有点热 "
+        return "今天可能很热 出门要做好防晒哦 "
 
 # ================= 生日倒计时 =================
 def birthday_countdown():
@@ -94,10 +95,11 @@ def main():
 
     title = "🌤 奥克兰 · 今日提醒"
 
+    birthday_line = "Mio宝宝的生日就是今天！\n\n" if days == 0 else f"距离 Mio 宝宝的生日还有 {days} 天\n\n"
     body = (
         f"奥克兰今天 {weather} {temp}℃\n"
         f"{care}\n\n"
-        f"距离 Mio 宝宝的生日还有 {days} 天\n\n"
+        f"{birthday_line}"
         f"{words}"
     )
 
